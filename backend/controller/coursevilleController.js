37675b207c4cf351f035eaf232d1e94bde01a3e5
Mond45 <mondc45@gmail.com>
2023-04-30 20:03:57 +0700

    backend finished

--- backend/controller/coursevilleController.js
+++ backend/controller/coursevilleController.js
@@ -13,6 +13,8 @@ exports.authApp = (req, res) => {
 };
 
 exports.accessToken = (req, res) => {
+  console.log("afjonaoijfaoi")
+  console.log(req)
   const parsedUrl = url.parse(req.url);
   const parsedQuery = querystring.parse(parsedUrl.query);
 
@@ -53,7 +55,7 @@ exports.accessToken = (req, res) => {
           console.log(req.session);
           if (token) {
             res.writeHead(302, {
-              Location: `http://${process.env.frontendIPAddress}/home.html`,
+              Location: `http://${process.env.frontendIPAddress}/index.html`,
             });
             res.end();
           }
@@ -107,28 +109,106 @@ exports.getProfileInformation = (req, res) => {
 // TODO #3.2: Send "GET" request to CV endpoint to get all courses that you enrolled
 exports.getCourses = (req, res) => {
   // You should change the response below.
-  res.send("This route should get all courses that you enrolled.");
-  res.end();
+  try {
+    const profileOptions = {
+      headers: {
+        Authorization: `Bearer ${req.session.token.access_token}`,
+      },
+    };
+    const profileReq = https.request(
+      "https://www.mycourseville.com/api/v1/public/get/user/courses",
+      profileOptions,
+      (profileRes) => {
+        let profileData = "";
+        profileRes.on("data", (chunk) => {
+          profileData += chunk;
+        });
+        profileRes.on("end", () => {
+          const profile = JSON.parse(profileData);
+          res.send(profile);
+          res.end();
+        });
+      }
+    );
+    profileReq.on("error", (err) => {
+      console.error(err);
+    });
+    profileReq.end();
+  } catch (error) {
+    console.log(error);
+    console.log("Please logout, then login again.");
+  }
 };
 
 // TODO #3.4: Send "GET" request to CV endpoint to get all course assignments based on cv_cid
 exports.getCourseAssignments = (req, res) => {
-  const cv_cid = req.params.cv_cid;
-  // You should change the response below.
-  res.send("This route should get all course assignments based on cv_cid.");
-  res.end();
+  try {
+    const profileOptions = {
+      headers: {
+        Authorization: `Bearer ${req.session.token.access_token}`,
+      },
+    };
+    const profileReq = https.request(
+      "https://www.mycourseville.com/api/v1/public/users/me",
+      profileOptions,
+      (profileRes) => {
+        let profileData = "";
+        profileRes.on("data", (chunk) => {
+          profileData += chunk;
+        });
+        profileRes.on("end", () => {
+          const profile = JSON.parse(profileData);
+          res.send(profile);
+          res.end();
+        });
+      }
+    );
+    profileReq.on("error", (err) => {
+      console.error(err);
+    });
+    profileReq.end();
+  } catch (error) {
+    console.log(error);
+    console.log("Please logout, then login again.");
+  }
 };
 
 // Outstanding #2
 exports.getAssignmentDetail = (req, res) => {
   const itemid = req.params.item_id;
-  // You should change the response below.
-  res.send("This route should get assignment details based on item_id.");
-  res.end();
+  try {
+    const profileOptions = {
+      headers: {
+        Authorization: `Bearer ${req.session.token.access_token}`,
+      },
+    };
+    const profileReq = https.request(
+      `https://www.mycourseville.com/api/v1/public/get/item/assignment?item_id=${itemid}`,
+      profileOptions,
+      (profileRes) => {
+        let profileData = "";
+        profileRes.on("data", (chunk) => {
+          profileData += chunk;
+        });
+        profileRes.on("end", () => {
+          const profile = JSON.parse(profileData);
+          res.send(profile);
+          res.end();
+        });
+      }
+    );
+    profileReq.on("error", (err) => {
+      console.error(err);
+    });
+    profileReq.end();
+  } catch (error) {
+    console.log(error);
+    console.log("Please logout, then login again.");
+  }
 };
 
 exports.logout = (req, res) => {
-  req.session.destroy();
-  res.redirect(`http://${process.env.frontendIPAddress}/login.html`);
-  res.end();
+  req.session.destroy()
+  res.redirect(`http://${process.env.frontendIPAddress}/index.html`)
+  res.end()
 };
--- backend/controller/itemsController.js
+++ backend/controller/itemsController.js
@@ -10,9 +10,26 @@ const {
 
 const docClient = new DynamoDBClient({ regions: process.env.AWS_REGION });
 
-exports.getGroupMembers = async (req, res) => {
+// exports.getGroupMembers = async (req, res) => {
+//   const params = {
+//     TableName: process.env.aws_group_members_table_name,
+//   };
+//   try {
+//     const data = await docClient.send(new ScanCommand(params));
+//     res.send(data.Items);
+//   } catch (err) {
+//     console.error(err);
+//     res.status(500).send(err);
+//   }
+// };
+
+const tableName = "items_db_99"
+
+// TODO #1.1: Get items from DynamoDB
+exports.getItems = async (req, res) => {
+  // You should change the response below.
   const params = {
-    TableName: process.env.aws_group_members_table_name,
+    TableName: tableName
   };
   try {
     const data = await docClient.send(new ScanCommand(params));
@@ -21,20 +38,27 @@ exports.getGroupMembers = async (req, res) => {
     console.error(err);
     res.status(500).send(err);
   }
-};
-
-// TODO #1.1: Get items from DynamoDB
-exports.getItems = async (req, res) => {
-  // You should change the response below.
   res.send("This route should get all items in DynamoDB.");
 };
 
 // TODO #1.2: Add an item to DynamoDB
 exports.addItem = async (req, res) => {
   const item_id = uuidv4();
-  const created_date = Date.now();
-  const item = { item_id: item_id, ...req.body, created_date: created_date };
+  const item = { item_id: item_id, ...req.body };
 
+  // You should change the response below.
+  const putParams = {
+    TableName: tableName,
+    Item: item, // member object to be added
+  }
+  try {
+    // use 'PutCommand' object to insert an item into the table
+    await docClient.send(new PutCommand(putParams));
+  }
+  catch (err) {
+    console.error(err);
+    return;
+  }
   // You should change the response below.
   res.send("This route should add an item in DynamoDB.");
 };
@@ -43,6 +67,17 @@ exports.addItem = async (req, res) => {
 exports.deleteItem = async (req, res) => {
   const item_id = req.params.item_id;
 
+  const deleteParams = {
+    TableName: process.env.aws_items_table_name,
+    Key: { item_id: item_id }
+  }
+  try {
+    const response = await docClient.send(new DeleteCommand(deleteParams));
+    console.log(response);
+  } catch (err) {
+    console.error(err)
+    return;
+  }
   // You should change the response below.
   res.send("This route should delete an item in DynamoDB with item_id.");
 };
