beed85e6b6a9422cea8e2f8f1cd29a4279ecfbaf
Mond45 <mondc45@gmail.com>
2023-05-03 03:41:08 +0700

    final

--- frontend/script.js
+++ frontend/script.js
@@ -30,18 +30,15 @@ const fetchUsername = async () => {
         .catch((error) => console.log(error));
 }
 
-/*
-const options = {
-        method: "GET",
-        credentials: "include",
-    };
-await fetch(`http://${backendIPAddress}/courseville/get_profile_info`, options)
-            .then((response) => response.json())
-            .then((data) => {
-                console.log(data)
-            })
-            .catch((error) => console.log(error));
-*/
+const yyyymmdd = (date) => {
+    var mm = date.getMonth() + 1; // getMonth() is zero-based
+    var dd = date.getDate();
+
+    return [date.getFullYear(),
+    (mm > 9 ? '' : '0') + mm,
+    (dd > 9 ? '' : '0') + dd
+    ].join('-');
+};
 
 const fetchAssignmentDetail = async (itemId) => {
     const options = {
@@ -62,10 +59,20 @@ const fetchClassTasks = async (classId) => {
     let className = await fetchClassName(classId)
     let data = await fetch(`http://${backendIPAddress}/courseville/get_course_assignments/${classId}`, options)
         .then((response) => response.json())
+    const now = yyyymmdd(new Date())
     for (let e of data["data"]) {
         let id = e["itemid"]
         const task = await fetchAssignmentDetail(id)
-        tasks.push({ "date": task["data"]["duedate"], "title": task["data"]["title"], "desc": className })
+        const f = { "date": task["data"]["duedate"], "title": task["data"]["title"], "desc": className }
+        tasks.push(f)
+        if (f["date"] >= now) {
+            let tmp = `<div class="box">`;
+            tmp += `<h1>${f["title"]}</h1>`;
+            tmp += `<p>${f["desc"]}</p>`;
+            tmp += `<p>${f["date"]}</p>`;
+            tmp += `</div>`;
+            document.getElementById("cardList").innerHTML += tmp
+        }
     }
     return tasks
 }
@@ -81,7 +88,6 @@ const fetchClassName = async (classId) => {
 }
 
 const fetchMCVTasks = async () => {
-
     const options = {
         method: "GET",
         credentials: "include",
@@ -113,6 +119,16 @@ const fetchDatabase = async () => {
     return data
 }
 
+//!!
+const validateFill = () => {
+    const task = document.getElementById("txt_task").value.trim()
+    if (!task) {
+        return false
+    }
+
+    return true
+}
+
 const updateItem = async (itemId, title, desc, date) => {
     const data = {
         title: title,
@@ -129,16 +145,6 @@ const updateItem = async (itemId, title, desc, date) => {
     };
     await fetch(`http://${backendIPAddress}/items/${itemId}/${username}`, options)
 }
-
-// const doStuff = async () => {
-//     let l = await fetchMCVTasks().then((data) => data)
-//     // let l = await fetchClassName("32205")
-//     // l.sort((a, b) => a["date"] - b["date"])
-//     console.log(l)
-// }
-
-// fetchUsername()
-
 
 const removeItem = async (itemId) => {
     const options = {
@@ -163,4 +169,155 @@ const createItem = async (title, desc, date) => {
         },
     }
     await fetch(`http://${backendIPAddress}/items/${username}`, options)
+}
+
+let id_editing = ""
+let state = ""
+let logged_in = false
+
+let filtered = []
+
+function closePopup() {
+    document.getElementById("popup").style.display = "none";
+    id_editing = ""
+    state = ""
+}
+
+function showPopup2(mode, id = "") { //mode will be implement by other function
+    const popup = document.getElementById("popup");
+    if (mode === "create") {
+        state = "create"
+        document.getElementById("btn_delete").style.display = "none"
+        document.getElementById("dialog_title").innerText = "Create Task"
+        document.getElementById("txt_task").value = "";
+        document.getElementById("txt_desc").value = "";
+        document.getElementById("date_picker").value = yyyymmdd(new Date());
+    } else if (mode === "edit") {
+        id_editing = id
+        state = "edit"
+        const task = data.find((e) => e.item_id === id_editing)
+        document.getElementById("btn_delete").style.display = "block"
+        document.getElementById("dialog_title").innerText = "Edit Task"
+        document.getElementById("txt_task").value = task.title;
+        document.getElementById("txt_desc").value = task.desc;
+        document.getElementById("date_picker").value = task.date;
+    }
+    popup.style.display = "block";
+}
+
+async function saveChanges() {
+    const title = document.getElementById("txt_task").value;
+    const desc = document.getElementById("txt_desc").value;
+    const date = document.getElementById("date_picker").value;
+
+    if (state === "edit") {
+        console.log("EDITING")
+        await updateItem(id_editing, title, desc, date);
+    } else if (state === "create") {
+        await createItem(title, desc, date);
+    }
+}
+
+const submitHandler = async () => {
+    if (validateFill()) {
+        console.log("OK!")
+        await saveChanges()
+        await updateData();
+        await searchHandler();
+        await updateUI();
+        closePopup();
+    } else {
+        alert("Fill in things first!!")
+    }
+}
+
+const deleteHandler = async () => {
+    await removeItem(id_editing);
+    await updateData();
+    await searchHandler();
+    await updateUI();
+    closePopup();
+}
+
+let data1 = [];
+let data2 = [];
+let data = []
+
+const searchHandler = () => {
+    const searchValue = document.getElementById("searchbox").value.trim().toLowerCase();
+    updateFilter(searchValue);
+    updateUI();
+}
+
+const updateData = async (mcv = false) => {
+    data1 = await fetchDatabase()
+    if (mcv) {
+        document.getElementById("cardList").innerHTML = ""
+        data2 = await fetchMCVTasks()
+        document.getElementById("cardList").innerHTML = ""
+    }
+    data = data1.concat(data2)
+    const now = yyyymmdd(new Date())
+    console.log(now)
+    data = data.filter((x) => {
+        console.log(x)
+        return x.date >= now
+    })
+}
+
+const updateFilter = (searchTerm) => {
+    if (searchTerm === "") {
+        filtered = data;
+    } else {
+        console.log("update filter hit")
+        filtered = data.filter((item) => {
+            return (
+                item.title.toLowerCase().includes(searchTerm) ||
+                item.desc.toLowerCase().includes(searchTerm) ||
+                item.date.toLowerCase().includes(searchTerm)
+            );
+        });
+    }
+}
+
+// updateData updateFilter
+
+const updateUI = async (mcv = false) => {
+    const innerHTML = filtered.sort((a, b) =>
+        a.date.localeCompare(b.date)
+    ).map((e) => {
+        let tmp = `<div class="box" ${e.item_id !== undefined ? `onclick="showPopup2('edit','${e.item_id}')" style="cursor:pointer;"` : ""}>`;
+        tmp += `<h1>${e["title"]}</h1>`;
+        tmp += `<p>${e["desc"]}</p>`;
+        tmp += `<p>${e["date"]}</p>`;
+        tmp += `</div>`;
+        return tmp;
+    }).join('')
+    console.log(innerHTML)
+    document.getElementById("cardList").innerHTML = innerHTML;
+}
+
+const init = async () => {
+    await fetchUsername();
+    if (logged_in) {
+        const logbtn = document.getElementById("btn_login")
+        logbtn.innerText = "Logout"
+        logbtn.onclick = () => {
+            logout()
+        }
+    } else {
+        document.getElementById("searchbox").style.display = 'none'
+        document.getElementById("btn_green").style.display = 'none'
+        document.getElementById("main_title").innerText = "Please Login with your MCV account first."
+
+    }
+    document.getElementById("loading_text").style.display = 'block'
+    document.getElementById("btn_green").style.display = 'none'
+    document.getElementById("searchbox").disabled = true;
+    await updateData(true) //change this back to TRUE
+    document.getElementById("loading_text").style.display = 'none'
+    document.getElementById("btn_green").style.display = 'block'
+    document.getElementById("searchbox").disabled = false;
+    await updateFilter("")
+    await updateUI()
 }
